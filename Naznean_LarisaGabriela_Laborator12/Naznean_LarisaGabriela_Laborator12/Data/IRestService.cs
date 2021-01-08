12fc54dd2d94d936fc5866892e22a7875c3fb964
Larisa Gabriela Naznean <nazneanlarisa@gmail.com>
2021-01-08 21:04:22 +0200

    Add project files.

---
+++ Naznean_LarisaGabriela_Laborator12/Naznean_LarisaGabriela_Laborator12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Naznean_LarisaGabriela_Laborator12.Models;
+
+namespace Naznean_LarisaGabriela_Laborator12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
